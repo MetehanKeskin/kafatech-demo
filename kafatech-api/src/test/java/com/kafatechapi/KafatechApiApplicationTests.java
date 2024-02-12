c8499106a59a5249cc79e0941936d1964e194aec
MetehanKeskin <metehankkeskin06@gmail.com>
2024-02-13 02:44:03 +0300

    proje olusturuldu

---
+++ kafatech-api/src/test/java/com/kafatechapi/KafatechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafatechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafatechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
